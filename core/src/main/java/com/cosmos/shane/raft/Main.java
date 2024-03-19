fd83a0fd6f2440cfc87fac84e61da171c1166d1c
shane <Zzfx_7@163.com>
2024-03-19 23:03:39 +0800

    build: initial
    - hello world

---
+++ core/src/main/java/com/cosmos/shane/raft/Main.java
@@ -0,0 +1,13 @@
+package com.cosmos.shane.raft;
+
+/**
+ * @author Shane
+ * @version 1.0
+ * @date 2024-03-19 23:02
+ */
+public class Main {
+    public static void main(String[] args) {
+        //
+        System.out.println("Hello world!");
+    }
+}
